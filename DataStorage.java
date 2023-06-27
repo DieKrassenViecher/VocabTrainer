441f139ed58a11331c34b5176749c89683962d47
Thore Müller <75487485+LUPLUV@users.noreply.github.com>
2023-06-27 10:26:39 +0200

    ftrdsg

--- DataStorage.java
+++ DataStorage.java
@@ -2,40 +2,64 @@ import java.util.*;
 import java.io.*;
 
 /**
- * @author 
- * @version 
+ * @author Thore, Tommy
+ * @version 1.0
  */
 public class DataStorage
 {
-    // Bezugsobjekte
-    
-    // Attribute
+    /**
+     * Attribut für die Datei vokabeln.txt
+     */
     File storageFile;
 
     // Konstruktor
     public DataStorage()
     {
+        /**
+         * Das Attribut wird initialisiert.
+         */
         storageFile = new File("vokabeln.txt");
+        /**
+         * Falls die Datei vokabeln.txt nicht existiert, wird diese erstellt und ein Beispiel Vokabel reingesetzt.
+         */
         if(!storageFile.exists()) {
             try {
                 storageFile.createNewFile();
-            } catch (IOException ex) {
+                save("Nüsse:Nuts;Gehen:Walk");
+            } catch (IOException ex) /* Falls ein Fehler bei der Erstellung der 
+                                        Datei passiert wird er in der Konsole ausgegeben */ {
                 ex.printStackTrace();
             }
         }
     }
 
-    // Dienste
+    /**
+     * Speichert neuen Inhalt in die Datei vokabeln.txt
+     * @param String Der zu speichernde Inhalt
+     */
     public void save(String content) {
         try {
+            /**
+             * Es wird der FileWriter von Java benutzt
+             * um den Inhalt in die Datei zu schreiben.
+             */
             FileWriter fw = new FileWriter(storageFile);
             fw.write(content);
+            /**
+             * Durch das close() wird noch ein flush durchgeführt das alle Daten im Buffer wegschreibt.
+             * Dadurch werden Resourcen geschont.
+             */
             fw.close();
-        } catch (Exception ex) {
+        } catch (Exception ex) /* Falls ein Fehler bei der Erstellung der 
+                                  Datei passiert wird er in der Konsole ausgegeben */ {
             ex.printStackTrace();
         }
     }
     
+    /**
+     * Lädt den Inhalt von vokabeln.txt
+     * @return Der Inhalt der Datei als String
+     */
     public String read() {
         StringBuilder sb = new StringBuilder();
         try (FileReader fr = new FileReader(storageFile))
--- Vokabeln.java
+++ Vokabeln.java
@@ -1,15 +1,25 @@
 import java.util.*;
 /**
- * @author 
- * @version 
+ * @author Thore, Tommy
+ * @version 1.0
  */
 public class Vokabeln
 {
-    // [deutschwort|fremdwort];[deutschwort|fremdwort]
-    // Bezugsobjekte
+    // Speicher Schema der Vokabeln: deutschwort:fremdwort;deutschwort:fremdwort
+    /**
+     * Die Liste Dient als Cache für die Vokabeln, würde man bei
+     * jedem Vokabel die Datei neu aufrufen, würde das die Effizienz
+     * des Programms immenz reduzieren.
+     */
     public static List<Vokabeln> loadedWords = new ArrayList<>();
+    
     public static DataStorage dataStorage = null;
     
+    /**
+     * Wird genutzt um das DataStorage Objekt zu initialisieren
+     * falls es noch nicht initialisiert ist.
+     * @return Das Objekt der Klasse DataStorage
+     */
     public static DataStorage getData() {
         if(dataStorage == null) {
             dataStorage = new DataStorage();
@@ -20,6 +30,7 @@ public class Vokabeln
     // Attribute
     protected String Fremdwort;
     protected String Deutschwort;
+    
     // Konstruktor
     public Vokabeln(String Deutschwort, String Fremdwort)
     {
@@ -27,18 +38,46 @@ public class Vokabeln
         this.Deutschwort = Deutschwort;
     }
 
-    // Dienste
+    /**
+     * Lädt alle Wörter aus der Datei vokabeln.txt
+     */
     public static void loadWords() {
+        /**
+         * Zuerst wird der aktuelle Cache geleert.
+         */
         loadedWords.clear();
+        /**
+         * Anschließend wird der Dateiinhalt mit der Methode DataStorage#read
+         * ausgelesen und in content gespeichert.
+         */
         String content = getData().read();
+        /**
+         * Der Dateiinhalt wird in die einzelnen Übersetzungen aufgeteilt und im
+         * String Array words gespeichert.
+         */
         String[] words = content.split(";");
+        /**
+         * Die For Schleife geht alle Übersetzungen durch und fügt sie zum Cache hinzu.
+         */
         for(String word : words) {
+            /**
+             * Die Übersetzung wird in Deutschwort und Fremdwort aufgeteilt
+             * und im String Array splitted gespeichert.
+             */
             String[] splitted = word.split(":");
+            /**
+             * Es wird ein Objekt Vokabeln erstellt mit den Deutschwort und Fremdwort
+             * als Parameter und anschließend zum Cache hinzugefügt.
+             */
             Vokabeln vok = new Vokabeln(splitted[0], splitted[1]);
             loadedWords.add(vok);
         }
     }
     
+    /**
+     * Fügt ein Wort zum Cache und zur Datei vokabeln.txt hinzu.
+     * @param Vokabeln Vokabel das hinzugefügt werden soll
+     */
     public static void addWord(Vokabeln vok) {
         loadedWords.add(vok);
         getData().save(getData().read() + ";" + vok.Deutschwort + ":" + vok.Fremdwort);
